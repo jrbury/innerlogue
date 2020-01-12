cd6e1339f186bfd781f8cddf8b3e0387e9ff3ca3
Jerod Bury <jerod.bury@gmail.com>
2020-01-12 20:19:54 +0000

    react

---
+++ constants/tabs.js
@@ -0,0 +1,9 @@
+let tabs;
+export default tabs = {
+  categories: [
+    { id: 'popular', title: 'Popular' },
+    { id: 'beauty', title: 'Beauty' },
+    { id: 'fashion', title: 'Fashion' },
+    { id: 'car_motorcycle', title: 'Car & Motorcycle' },
+  ],
+}
